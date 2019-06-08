23eb61a695baf3607240e7f64fec5a2ba80727eb
j3kaiii <j3kaiii@gmail.com>
2019-06-08 17:17:52 +0300

    lesson1.Fruit boxes

--- lesson1/FruitBoxes.java
+++ lesson1/FruitBoxes.java
@@ -1,44 +1,60 @@
 package lesson1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FruitBoxes {
     public static void main(String[] args) {
         Box<Apple> appleBox = new Box<Apple>();
         appleBox.addFruit(new Apple());
+        appleBox.addFruit(new Apple());
+        appleBox.addFruit(new Apple());
+        appleBox.addFruit(new Apple());
+        appleBox.addFruit(new Apple());
+        //appleBox.addFruit(new Orange());  -- не работает, этот бокс только для яблок
 
         Box<Orange> orangeBox = new Box<Orange>();
         orangeBox.addFruit(new Orange());
+        orangeBox.addFruit(new Orange());
+        orangeBox.addFruit(new Orange());
+        orangeBox.addFruit(new Orange());
+        //orangeBox.addFruit(new Apple());  -- не работает, этот бокс только для апельсинов
+
+        System.out.println("Apple box weight : " + appleBox.getWeight());
+        System.out.println("Orange box weight : " + orangeBox.getWeight());
+
+        Box<Apple> appleBox1 = new Box<>();
+        appleBox.moveToOtherBox(appleBox1);
+        //orangeBox.moveToOtherBox(appleBox1);  -- не работает, разные типы боксов
+        System.out.println("Old apple box weight : " + appleBox.getWeight());
+        System.out.println("New Apple box weight : " + appleBox1.getWeight());
     }
 }
 
 abstract class Fruit {
     public abstract float getWeight();
-
 }
 
 class Apple extends Fruit {
-    static float WEIGHT = 1.0f;
+    float weight = 1.0f;
 
     public float getWeight() {
-        return WEIGHT;
+        return weight;
     }
 }
 
 class Orange extends Fruit {
-    static float WEIGHT = 1.5f;
+    static float weight = 1.5f;
 
     public float getWeight() {
-        return WEIGHT;
+        return weight;
     }
 }
 
 class Box<T> {
     private ArrayList<T> list;
-    private float weight;
 
     public Box() {
-        this.weight = 0.0f;
         this.list = new ArrayList<T>();
     }
 
@@ -46,7 +62,24 @@ class Box<T> {
         list.add(fruit);
     }
 
+    public void moveToOtherBox(Box<T> otherBox) {
+        for (T obj : list) {
+            otherBox.addFruit(obj);
+        }
+        list.clear();
+    }
+
     public float getWeight() {
-        return weight; // list.size() * <T>.getWeight - не работает, не известен объект Т
+        float weight = 0.0f;
+        if (!list.isEmpty()) {
+            Fruit f = (Fruit)list.get(0);
+            weight =  list.size() * f.getWeight();
+        }
+        return weight;
+    }
+
+    public boolean compare(Box<?> otherBox) {
+        if (this.getWeight() == otherBox.getWeight()) return true;
+        return false;
     }
 }
