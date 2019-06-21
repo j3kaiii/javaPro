aab41aaab4964c3cafd47b22bc9889aa0deef403
j3kaiii <j3kaiii@gmail.com>
2019-06-21 23:59:56 +0300

    added Semaphore, CyclicBarier

--- lesson5/Car.java
+++ lesson5/Car.java
@@ -1,7 +1,13 @@
 package lesson5;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    CyclicBarrier cbar;
+    Semaphore sem;
+    Win w;
     static {
         CARS_COUNT = 0;
     }
@@ -14,7 +20,10 @@ public class Car implements Runnable {
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier c, Semaphore s, Win win) {
+        w = win;
+        sem = s;
+        cbar = c;
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
@@ -26,11 +35,28 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cbar.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+
+        if (w.isWin()) {
+            System.out.println(this.name + " WIN");
+        }
+    }
+}
+
+class Win {
+    boolean win = true;
+
+    synchronized boolean isWin() {
+        if (win) {
+            win = false;
+            return true;
+        }
+        return false;
     }
 }
--- lesson5/MainClass.java
+++ lesson5/MainClass.java
@@ -1,22 +1,40 @@
 package lesson5;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Thread[] threads = new Thread[CARS_COUNT];
+        Win win = new Win();
+        CyclicBarrier cb = new CyclicBarrier(4, MainClass::startInfo);
+        Semaphore s = new Semaphore(CARS_COUNT/2);
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb, s, win);
         }
         for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+            threads[i] = new Thread(cars[i]);
+            threads[i].start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+
+        try {
+            for (Thread t: threads) {
+                t.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\nВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+
+    static void startInfo() {
+        System.out.println("\nВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!\n");
     }
 }
 
