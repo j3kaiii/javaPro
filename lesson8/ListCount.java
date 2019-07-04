35fee0cd035e14228530bac0fda8b1f5759a64d5
j3kaiii <j3kaiii@gmail.com>
2019-07-04 23:02:39 +0300

    добавлена обертка для классов в коллекции

--- lesson8/ListCount.java
+++ lesson8/ListCount.java
@@ -1,18 +1,39 @@
 package lesson8;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
+class MyObject<E> {
+    private E obj;
 
-public class ListCount<T> {
+    public MyObject(E obj) {
+        this.obj = obj;
+    }
 
-    public void ListObjectsCount(ArrayList<T> list) {
+    public E getObj() {
+        return obj;
+    }
+
+    public void showType() {
+        System.out.println("Тип объекта : " + obj.getClass().getName());
+    }
+}
+
+
+public class ListCount {
+
+    public void ListObjectsCount(ArrayList<MyObject> list) {
             int count = 0;
 
             if (!list.isEmpty()) {
-                Iterator<T> iter = list.iterator();
+                Iterator<MyObject> iter = list.iterator();
                 while (iter.hasNext()) {
+                    MyObject o = iter.next();
+                    o.showType();
+                    System.out.println(o.getObj() + "\n");
                     count++;
-                    iter.next();
+
                 }
             }
         System.out.println("Count: " + count);
@@ -23,8 +44,32 @@ public class ListCount<T> {
 class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(9,8,7,6,5,4,3));
+//        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(9,8,7,6,5,4,3));
+//
+        ArrayList<MyObject> list = new ArrayList<>();
+        list.add(new MyObject(10));
+        list.add(new MyObject("Some string"));
+        list.add(new MyObject(true));
+        list.add(new MyObject(new Cat("Barsik")));
+
         ListCount lc = new ListCount();
         lc.ListObjectsCount(list);
+    }
+}
+
+class Cat {
+    String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat name " + getName();
     }
 }
